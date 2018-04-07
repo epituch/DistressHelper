ebe0ebf1720f2786252447836217c984e3ee5070
epituch <epituch@purdue.edi>
2018-04-07 16:54:20 -0400

    added getting random pic again

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 console.log("JS Loaded");
-let grillPics = [];
-let coughPics = [];
-let casePics = [];
-let bandagePics = [];
+let grillPics = ["grill0.jpg","grill1.jpg","grill2.jpg","grill3.jpg","grill4.jpg","grill5.jpg","grill6.jpg","grill7.jpg","grill8.jpg","grill9.jpg"];
+let coughPics = ["cough0.jpg","cough1.jpg","cough2.jpg","cough3.jpg","cough4.jpg","cough5.jpg","cough6.jpg","cough7.jpg","cough8.jpg","cough9.jpg"];
+let castPics = ["cast0.jpg","cast1.jpg","cast2.jpg","cast3.jpg","cast4.jpg","cast5.jpg","cast6.jpg","cast7.jpg","cast8.jpg","cast9.jpg"];
+let bandagePics = ["bandage0.jpg","bandage1.jpg","bandage2.jpg","bandage3.jpg","bandage4.jpg","bandage5.jpg","bandage6.jpg","bandage7.jpg","bandage8.jpg","bandage9.jpg"];
 let allPics = [grillPics, coughPics, casePics, bandagePics];
 function grillClicked(){
 	getRandomPic();
@@ -20,5 +20,5 @@ function bandageClicked(){
 function getRandomPic(){
 	let x = Math.floor(Math.random() * 4);
 	let y = Math.floor(Math.random() * 10);
-	document.getElementById("changingImage").src = "";
+	document.getElementById("changingImage").src = allPics[x][y];
 };
