955fed30cfd12cac05cb17dbde0c7a96804f3956
Huberrrr <hubermjonathan@gmail.com>
2018-04-07 17:08:20 -0400

    fixed js and added score to html

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@ let grillPics = ["grill0.jpg","grill1.jpg","grill2.jpg","grill3.jpg","grill4.jpg
 let coughPics = ["cough0.jpg","cough1.jpg","cough2.jpg","cough3.jpg","cough4.jpg","cough5.jpg","cough6.jpg","cough7.jpg","cough8.jpg","cough9.jpg"];
 let castPics = ["cast0.jpg","cast1.jpg","cast2.jpg","cast3.jpg","cast4.jpg","cast5.jpg","cast6.jpg","cast7.jpg","cast8.jpg","cast9.jpg"];
 let bandagePics = ["bandage0.jpg","bandage1.jpg","bandage2.jpg","bandage3.jpg","bandage4.jpg","bandage5.jpg","bandage6.jpg","bandage7.jpg","bandage8.jpg","bandage9.jpg"];
-let allPics = [grillPics, coughPics, casePics, bandagePics];
+var allPics = [grillPics, coughPics, castPics, bandagePics];
+var scoreCounter = 0;
+
 function grillClicked(){
-	getRandomPic();
+  let category = getRandomPic();
+  if(category == 0) {
+    scoreCounter++;
+    document.getElementById("score").innerHTML = "Your Doctor Score: " + scoreCounter;
+  }
 	console.log("Clicked");
 };
 function coughClicked(){
@@ -21,4 +27,5 @@ function getRandomPic(){
 	let x = Math.floor(Math.random() * 4);
 	let y = Math.floor(Math.random() * 10);
 	document.getElementById("changingImage").src = allPics[x][y];
+  return x;
 };
